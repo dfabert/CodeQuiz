11ef2a2b418d2bc467b386be881c2b66428b0acf
dfabert <glen.fabert@gmail.com>
2020-08-01 22:47:52 -0400

    added pseudo coding to js and stying to html

---
+++ script.js
@@ -0,0 +1,28 @@
+//Define questions with arrays
+//Arrays are set up in this order
+//0-Question
+//1-Option 1
+//2-Option 2
+//3-Option 3
+//4-Option 4
+//5-Number of correct answer
+
+var Q1 = ['The first question will go here and the answers will line up below', 'Answer1', 'Answer2', 'Answer3', 'Answer4', 3];
+var Q2 = ['This is the second question and it should be about this long or so', 'Answer1', 'Answer2', 'Answer3', 'Answer4', 1];
+
+//Listen for Button to be clicked
+var start = document.getElementById('start');
+start.addEventListener('click', function(){
+    //Start Timer
+
+    //Loop to display questions
+
+    //variable to calculation correct answers
+
+    //Create Results Page
+
+}, false);
+
+
+
+
