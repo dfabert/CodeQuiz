378a80aa5b53f9efaaee77ed6f6e292d93122245
dfabert <glen.fabert@gmail.com>
2020-08-02 16:11:51 -0400

    created function to collect user entry

--- script.js
+++ script.js
@@ -1,22 +1,17 @@
-//All Elements
 var start = document.getElementById('start');
 //var timer = 
 //var correct = 
 //var totalQuestions = 
-var question = document.getElementById('question')
-var option1 = document.getElementById('a1')
-var option2 = document.getElementById('a2')
-var option3 = document.getElementById('a3')
-var option4 = document.getElementById('a4')
+
 
 
 //Question Object
 var questions = [
-    { q:"The first Question will go here and it should be about this long", a: "Option #1", b: "Option #2", c: "Option #3", d: "Option #4", answer: 1},
-    { q:"The second Question will go here and it should be about this long", a: "Option #1", b: "Option #2", c: "Option #3", d: "Option #4", answer: 2},
-    { q:"The third Question will go here and it should be about this long", a: "Option #1", b: "Option #2", c: "Option #3", d: "Option #4", answer: 3},
-    { q:"The fourth Question will go here and it should be about this long", a: "Option #1", b: "Option #2", c: "Option #3", d: "Option #4", answer: 4},
-    { q:"The fifth Question will go here and it should be about this long", a: "Option #1", b: "Option #2", c: "Option #3", d: "Option #4", answer: 1},
+    { q:"The first Question will go here and it should be about this long", a: "Option #1", b: "Option #2", c: "Option #3", d: "Option #4", answer: 'a'},
+    { q:"The second Question will go here and it should be about this long", a: "Option #11", b: "Option #22", c: "Option #33", d: "Option #44", answer: 'b'},
+    { q:"The third Question will go here and it should be about this long", a: "Option #21", b: "Option #23", c: "Option #37", d: "Option #4", answer: 'c'},
+    { q:"The fourth Question will go here and it should be about this long", a: "Option #1", b: "Option #2", c: "Option #3", d: "Option #4", answer: 'd'},
+    { q:"The fifth Question will go here and it should be about this long", a: "Option #1", b: "Option #2", c: "Option #3", d: "Option #4", answer: 'e'},
         ];
 
 
@@ -27,16 +22,39 @@ var questions = [
         //Start Asking questions questions
         i = 0;
         console.log("It has begun");
-        //questionDisplay(i,questions);
-        //userAnswer(i,questions);
+        questionDisplay(i,questions);
     });
+//Display of Questions//
+function questionDisplay(){
+    console.log('questionDisplay');
+    var question = document.getElementById('question').innerHTML=questions[i].q;
+    var option1 = document.getElementById('a1').innerHTML=questions[i].a;
+    var option2 = document.getElementById('a2').innerHTML=questions[i].b;
+    var option3 = document.getElementById('a3').innerHTML=questions[i].c;
+    var option4 = document.getElementById('a4').innerHTML=questions[i].d;
+}
+
+//User Input and Scoring//
+function userAnswer(userAnswer){
+    console.log(userAnswer)
+    console.log(questions[i].answer);
+    
+    //check user answer
+    if(userAnswer == questions[i].answer){
+        console.log('they got it right');
+    }else{
+        console.log('they got it wrong');
+    }
+
+    i++ 
+    //if we keep going or not
+    if(i < questions.length){               
+        questionDisplay(i,questions);
+    }else{
+        console.log('we are done!');
+    }
+}
       
-//Functions called by question loop
 
-
-
- 
-    //variable to calculation correct answers
-
-    //Create Results Page
+//Create Results Page
 
